b68ff4a8513c834c2c6fd1f08113e657189e4498
Wiliam Chen <leobuzhi@gmail.com>
2016-03-27 11:21:28 +0800

    Create add.cpp
    No add , subtract , multiply and divide to come true Add.

---
+++ add.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int add(int a, int b)
+{
+    int sum = 0, tmp = 0;
+    do{
+        sum = a^b;
+        tmp = a&b;
+        b = tmp << 1;
+        a = sum;
+    } while (tmp);
+    return sum;
+}
+int main()
+{
+    cout << add(1, 3) << endl;
+    cout << add(5, 8) << endl;
+    cout << add(-2, 15) << endl;
+    cout << add(-4, -7) << endl;
+    return 0;
+}
