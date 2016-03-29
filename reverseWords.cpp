6a8a004420c9141cb297c5b78b2770be69084277
Wiliam Chen <leobuzhi@gmail.com>
2016-03-29 22:54:06 +0800

    Create reverseWords.cpp

---
+++ reverseWords.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void reverse(char* str)
+{
+    char* begin, *end, *ptr;
+    begin = end = ptr = str;
+    while (*ptr++ != '\0')
+    {
+        if (*ptr == '\0' || *ptr == ' ')
+        {
+            end = ptr - 1;
+            while (begin<end)
+                swap(*begin++,*end--);
+            begin = ptr + 1;
+        }
+    }
+    begin = str;
+    end = ptr - 2;
+    while (begin<end)
+        swap(*begin++,*end--);
+}
+int main()
+{
+    char str[100]="I am a student";
+    reverse(str);
+    cout << str << endl;
+    return 0;
+}
