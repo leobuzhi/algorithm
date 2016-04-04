06d34b599a396bb7f859a8b480f8fffa5cba816f
Wiliam Chen <leobuzhi@gmail.com>
2016-04-04 22:56:00 +0800

    Create ChineseChess.cpp

---
+++ ChineseChess.cpp
@@ -0,0 +1,33 @@
+方法一：
+#include<iostream>
+using namespace std;
+int main()
+{
+    char i = 81;
+    while (i--)
+    {
+        if (i / 9 % 3 != i % 9 % 3)
+            cout << i / 9 + 1 << "," << i % 9 + 1 << endl;
+    }
+    return 0;
+}
+
+方法二：
+#include<stdio.h>
+typedef struct C{
+    unsigned char a : 4;
+    unsigned char b : 4;
+}C;
+int main()
+{
+    C c;
+    for (c.a = 1; c.a <= 9; c.a++)
+    { 
+        for (c.b = 1; c.b <= 9; c.b++)
+        {
+            if (c.a % 3 != c.b % 3)
+                printf("%d,%d \n", c.a, c.b);
+        }
+    }
+    return 0;
+}
