c3c7621236685df121441a8712b59ffc75a358c7
Wiliam Chen <leobuzhi@gmail.com>
2016-05-07 17:13:49 +0800

    Find Minimum in Rotated Sorted Array

--- findMin.cpp
+++ findMin.cpp
@@ -3,6 +3,7 @@
 Find the minimum element.
 You may assume no duplicate exists in the array.*/
 
+//my Solution
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -26,6 +27,29 @@ public:
             }
         }
         return nums[left];
+    }
+};
+
+//better Solution
+class Solution {
+public:
+    int findMin(vector<int> &num) {
+        int start=0,end=num.size()-1;
+
+        while (start<end) {
+            if (num[start]<num[end])
+                return num[start];
+
+            int mid = (start+end)/2;
+
+            if (num[mid]>=num[start]) {
+                start = mid+1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return num[start];
     }
 };
 
