845ab007a9d980e01f15cc5aa985b50caef9c073
Wiliam Chen <leobuzhi@gmail.com>
2016-05-11 20:43:15 +0800

    Find the Duplicate Number

---
+++ findDuplicate.cpp
@@ -0,0 +1,45 @@
+/*Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+Note:
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.*/
+
+
+//my Solution 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        set<int> si;
+        for(int x:nums)
+            if(si.find(x)==si.end())
+                si.insert(x);
+            else
+                return x;
+    }
+};
+
+
+
+//better Solution
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums)
+    {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+
+        fast = 0;
+        while (fast != slow)
+        {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
+    }
+};
