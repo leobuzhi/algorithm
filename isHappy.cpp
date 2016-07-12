d85ee5d17774086876e9fc60be5d1689f3623fcb
Joey Chen <leobuzhi@gmail.com>
2016-07-12 22:56:52 +0800

    Happy Number(1)

---
+++ isHappy.cpp
@@ -0,0 +1,36 @@
+/*Write an algorithm to determine if a number is "happy".
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+Example: 19 is a happy number
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1*/
+
+
+
+
+//other's Solution 
+class Solution {
+public:
+   int digitSquareSum(int n) {
+        int sum = 0, tmp;
+        while (n) {
+            tmp = n % 10;
+            sum += tmp * tmp;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow, fast;
+        slow = fast = n;
+        do {
+            slow = digitSquareSum(slow);
+            fast = digitSquareSum(fast);
+            fast = digitSquareSum(fast);
+            if(fast == 1) return 1;
+        } while(slow != fast);
+         return 0;
+    }
+};
